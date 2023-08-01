cdffb6e93de3a9b4e6d014db4f80642099252cce
Renee Pinna <renee.pinna@gmail.com>
2023-08-01 14:21:24 -0600

    Install and configure sass

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,11 @@ module.exports = {
   "module": {
     "rules": [
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
             { loader: 'style-loader' },
-            { loader: 'css-loader' }
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' }
         ]
     },
       {
