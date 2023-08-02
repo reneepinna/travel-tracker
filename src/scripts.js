09c7600402d438bd13b1a2e6cdb808551dd77352
Renee Pinna <renee.pinna@gmail.com>
2023-08-02 10:50:24 -0600

    Add funtion to initialize data

--- src/scripts.js
+++ src/scripts.js
@@ -5,4 +5,22 @@ const dayjs = require('dayjs')
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
+import {
+  getApiData,
+  
+} from './apiCalls';
+
+// function initializeDataModel() {
+//   return 
+// }
+
+function initializeData(userID) {
+  Promise.all([
+    getApiData(`http://localhost:3001/api/v1/travelers/${userID}`),
+    getApiData('http://localhost:3001/api/v1/trips'),
+    getApiData('http://localhost:3001/api/v1/destinations')
+  ])
+}
+
+
 
