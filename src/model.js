8721ebffb0670225406bf0e223f5539d6fe34e4e
Renee Pinna <renee.pinna@gmail.com>
2023-08-02 12:40:54 -0600

    Add function to sort trips into groups

--- src/model.js
+++ src/model.js
@@ -1,4 +1,28 @@
 export function getUserTrips(trips, userID) {
-  return trips.filter(trip => trip.id === userID)
+  return trips.filter(trip => trip.userID === userID)
 }
 
+export function sortTripGroup(trips) {
+  const today = dayjs();
+
+  return trips.reduce(
+    (groups, trip) => {
+      const tripDate = dayjs(trip.date, 'YYYY/MM/DD');
+
+      if (tripDate.isBefore(today)) {
+        groups.pastGroup.push(trip);
+      } else if (trip.status === 'approved') {
+        groups.upcomingGroup.push(trip);
+      } else {
+        groups.pendingGroup.push(trip);
+      }
+
+      return groups;
+    },
+    {
+      pastGroup: [],
+      pendingGroup: [],
+      upcomingGroup: [],
+    },
+  );
+}
