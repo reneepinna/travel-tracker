2e7029b563bf830fab1b0206c819dcfd66e485ea
Renee Pinna <renee.pinna@gmail.com>
2023-08-04 14:38:35 -0600

    Style dashboard

--- src/dom-updates.js
+++ src/dom-updates.js
@@ -2,8 +2,12 @@ const dayjs = require('dayjs');
 
 const userGreet = document.getElementById('userGreet');
 const emptyMsgs = [...document.querySelectorAll('.trips-board__empty')];
-const boardGroups = [...document.querySelectorAll('.trips-board__group__list')];
-const costThisYearBox = document.getElementById('costThisYear')
+const boardGroups = [...document.querySelectorAll('.trips-board__group--list')];
+const costThisYearBox = document.getElementById('costThisYear');
+
+const tabBar = document.querySelector('.trips-board__tab-bar');
+const tabGroups = document.querySelectorAll('.trips-board__group');
+const tabs = document.querySelectorAll('.trips-board__tab');
 
 export function displayUserData(user) {
   userGreet.innerText = `Welcome ${user.name.split(' ')[0]}`;
@@ -11,13 +15,17 @@ export function displayUserData(user) {
 
 export function displayUserTrips(tripGroups, destinations) {
   boardGroups.forEach(group => (group.innerHTML = ''));
-  emptyMsgs.forEach(msg => (msg.style.display = 'block'));
+  emptyMsgs.forEach(msg => msg.classList.remove('hidden'));
 
   const groupNames = Object.keys(tripGroups);
 
   groupNames.forEach(groupName => {
     if (tripGroups[groupName].length !== 0) {
       renderTripCards(groupName, tripGroups[groupName], destinations);
+    }
+
+    if (groupName === "pendingGroup") {
+      renderTabNotification(tripGroups[groupName])
     }
   });
 }
@@ -26,7 +34,7 @@ function renderTripCards(groupName, trips, destinations) {
   const emptyMsg = emptyMsgs.find(msg => msg.id === `${groupName}Msg`);
   const group = boardGroups.find(group => group.id === groupName);
 
-  emptyMsg.style.display = 'none';
+  emptyMsg.classList.add('hidden');
 
   trips.forEach(trip => {
     const destination = destinations.find(
@@ -35,20 +43,60 @@ function renderTripCards(groupName, trips, destinations) {
 
     const date = dayjs(trip.date, 'YYYY/MM/DD');
 
-    group.innerHTML += `<article class='card'>
+    group.innerHTML += `<article id='${trip.id}' class='tripCard'>
       <img class='tripCard__img' src=${destination.image}/>
       <div class='tripCard__info'>
         <p class='tripCard__location'>${destination.destination}</p>
-        <p class='tripCard__text'>${date.format('MM/DD/YYYY')}</p>
-        <p class='tripCard__text'>${date
-          .add(trip.duration, 'day')
-          .format('MM/DD/YYYY')}</p>
-        <p class='tripCard__text'>${trip.travelers} people</p>
+        <p class='tripCard__date'>${date.format('MM/DD/YYYY')} - ${date
+      .add(trip.duration, 'day')
+      .format('MM/DD/YYYY')}</p>
+        <p class='tripCard__travlers'>${formatTravlerNumber( trip.travelers)}</p>
+          <button class='tripCard__btn'>+<button>
       <div>
     </article>`;
   });
 }
 
-export function displayCostThisYear(costThisYear) {
-  costThisYearBox.innerText = `$${costThisYear}.00`
+function formatTravlerNumber(num) {
+  switch (num) {
+    case 1:
+       return `Just you` 
+    case 2:
+      return `You and another`
+    default:
+      return `You and ${num - 1} others`
+  }
 }
+
+export function renderTabNotification(pendingTrips) {
+
+  if (pendingTrips.length !== 0) {
+    document.querySelector('.trips-board__tab__num').innerText = pendingTrips.length;
+  }
+ 
+
+}
+
+export function displayCostThisYear(costThisYear) {
+  costThisYearBox.innerText = `$${costThisYear}.00`;
+}
+
+tabBar.addEventListener('click', e => {
+  if (e.target.className === 'trips-board__tab') {
+    tabs.forEach(tab => {
+      if (tab.id === `${e.target.id}`) {
+        tab.classList.add('active');
+      } else {
+        tab.classList.remove('active');
+      }
+    });
+
+    tabGroups.forEach(tabGroup => {
+      if (tabGroup.id === `${e.target.id}Group`) {
+        tabGroup.classList.remove('hidden');
+      } else {
+        tabGroup.classList.add('hidden');
+      }
+    });
+  }
+});
