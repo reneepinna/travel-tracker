4536d38fbbd8885d0a042177826db8586b35b012
Renee Pinna <renee.pinna@gmail.com>
2023-08-04 17:35:24 -0600

    Add post request function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,32 @@
 export function getApiData(url, key) {
   return fetch(url)
     .then(response => response.json())
-    .then(data => data[key] || data)
+    .then(data => data[key] || data);
 }
 
+export function setApiData(
+  id,
+  userID,
+  destinationID,
+  travelers,
+  date,
+  duration,
+) {
+  return fetch('http://localhost:3001/api/v1/trips', {
+    method: 'POST',
+    body: JSON.stringify({
+      id,
+      userID,
+      destinationID,
+      travelers,
+      date,
+      duration,
+      status: 'pending',
+      suggestedAvtivities: [],
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then(response => console.log(response.json()))
+}
